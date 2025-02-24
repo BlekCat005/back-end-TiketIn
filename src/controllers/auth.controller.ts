650fbd05ad768cd59539af955f5a42256adc42d7
Muhamad Rizki <mrizki004@gmail.com>
2025-02-24 15:18:04 +0700

    course eps 12

---
+++ src/controllers/auth.controller.ts
@@ -0,0 +1,132 @@
+import { Request, Response } from "express";
+import * as Yup from "yup";
+import UserModel from "../models/user.model";
+import { encrypt } from "../utils/encryption";
+import { generateToken } from "../utils/jwt";
+import { IReqUser } from "../middlewares/auth.middleware";
+
+type TRegister = {
+  fullName: string;
+  userName: string;
+  email: string;
+  password: string;
+  confirmPassword: string;
+};
+
+type TLogin = {
+  identifier: string;
+  password: string;
+};
+
+const regiterValidatorSchema = Yup.object({
+  fullName: Yup.string().required(),
+  userName: Yup.string().required(),
+  email: Yup.string().email().required(),
+  password: Yup.string().required().min(8),
+  confirmPassword: Yup.string()
+    .required()
+    .min(8)
+    .oneOf([Yup.ref("password")], "Password does not match"),
+});
+
+export default {
+  async register(req: Request, res: Response) {
+    const { fullName, userName, email, password, confirmPassword } =
+      req.body as unknown as TRegister;
+
+    try {
+      await regiterValidatorSchema.validate({
+        fullName,
+        userName,
+        email,
+        password,
+        confirmPassword,
+      });
+
+      const result = await UserModel.create({
+        fullName,
+        userName,
+        email,
+        password,
+      });
+
+      res.status(200).json({
+        message: "success registration!",
+        data: result,
+      });
+    } catch (error) {
+      const err = error as unknown as Error;
+      res.status(400).json({
+        message: err.message,
+        data: null,
+      });
+    }
+  },
+
+  async login(req: Request, res: Response) {
+    const { identifier, password } = req.body as unknown as TLogin;
+    try {
+      const userByIdentifier = await UserModel.findOne({
+        $or: [
+          {
+            email: identifier,
+          },
+          {
+            userName: identifier,
+          },
+        ],
+      });
+
+      if (!userByIdentifier) {
+        return res.status(403).json({
+          message: "user not found",
+          data: null,
+        });
+      }
+
+      const validatePassword: boolean =
+        encrypt(password) === userByIdentifier.password;
+
+      if (!validatePassword) {
+        return res.status(403).json({
+          message: "user not found",
+          data: null,
+        });
+      }
+
+      const token = generateToken({
+        id: userByIdentifier._id,
+        role: userByIdentifier.role,
+      });
+
+      res.status(200).json({
+        message: "success login!",
+        data: token,
+      });
+    } catch (error) {
+      const err = error as unknown as Error;
+      res.status(400).json({
+        message: err.message,
+        data: null,
+      });
+    }
+  },
+
+  async me(req: IReqUser, res: Response) {
+    try {
+      const user = req.user;
+      const result = await UserModel.findById(user?.id);
+
+      res.status(200).json({
+        message: "success get user!",
+        data: result,
+      });
+    } catch (error) {
+      const err = error as unknown as Error;
+      res.status(400).json({
+        message: err.message,
+        data: null,
+      });
+    }
+  },
+};
--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,27 @@
 import express from "express";
 import router from "./routes/api";
+import bodyParser from "body-parser";
+import db from "./utils/database";
 
-const app = express();
-const PORT = 3000;
+async function init() {
+  try {
+    const result = await db();
 
-app.use("/api", router);
+    console.log("database status:", result);
 
-app.listen(PORT, () => {
-  console.log(`Server is running on http://localhost:${PORT}`);
-});
+    const PORT = 3000;
+    const app = express();
+
+    app.use(bodyParser.json());
+
+    app.use("/api", router);
+
+    app.listen(PORT, () => {
+      console.log(`Server is running on http://localhost:${PORT}`);
+    });
+  } catch (error) {
+    console.error(error);
+  }
+}
+
+init();
