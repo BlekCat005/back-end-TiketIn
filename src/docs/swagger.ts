73d509b7246b43d60e1797ef95b7f1e73d6dfb7e
Muhamad Rizki <siapasiapa005@gmail.com>
2025-02-25 14:38:29 +0700

    add api documentation for login and register

---
+++ src/docs/swagger.ts
@@ -0,0 +1,38 @@
+import swaggerAutogen from "swagger-autogen";
+
+const doc = {
+  info: {
+    version: "v0.0.1",
+    title: "Dokumentasi API Acara",
+    description: "Dokumentasi API Acara",
+  },
+  servers: [
+    {
+      url: "http://localhost:3000/api",
+      description: "Local server",
+    },
+    {
+      url: "https://back-end-acara-jet-eight.vercel.app/api",
+      description: "Deployed server",
+    },
+  ],
+  components: {
+    securitySchemes: {
+      bearerAuth: {
+        type: "http",
+        scheme: "bearer",
+      },
+    },
+    schemas: {
+      LoginRequest: {
+        identifier: "rizki",
+        password: "abc12345",
+      },
+    },
+  },
+};
+
+const outputFile = "./swagger_output.json";
+const endpointsFile = ["../routes/api.ts"];
+
+swaggerAutogen({ openapi: "3.0.0" })(outputFile, endpointsFile, doc);
