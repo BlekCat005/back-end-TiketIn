1c6ae57d52905e1d543560f6a6296c4e51ae09d2
Muhamad Rizki <siapasiapa005@gmail.com>
2025-05-07 16:26:37 +0700

    feat: order crud

--- src/controllers/event.controller.ts
+++ src/controllers/event.controller.ts
@@ -1,13 +1,13 @@
 import { Response } from "express";
 import { IPaginationQuery, IReqUser } from "../utils/interfaces";
 import response from "../utils/response";
-import EventModel, { eventDAO, TEvent } from "../models/event.model";
+import EventModel, { eventDAO, TypeEvent } from "../models/event.model";
 import { FilterQuery } from "mongoose";
 
 export default {
   async create(req: IReqUser, res: Response) {
     try {
-      const payload = { ...req.body, createdBy: req.user?.id } as TEvent;
+      const payload = { ...req.body, createdBy: req.user?.id } as TypeEvent;
 
       await eventDAO.validate(payload);
       const result = await EventModel.create(payload);
@@ -18,27 +18,39 @@ export default {
   },
   async findAll(req: IReqUser, res: Response) {
     try {
+      const buildQuery = (filter: any) => {
+        let query: FilterQuery<TypeEvent> = {};
+
+        if (filter.search) query.$text = { $search: filter.search };
+        if (filter.category) query.category = filter.category;
+        if (filter.isOnline) query.isOnline = filter.isOnline;
+        if (filter.isPublish) query.isPublish = filter.isPublish;
+        if (filter.isFeatured) query.isFeatured = filter.isFeatured;
+
+        return query;
+      };
+
       const {
-        limit = 10,
+        limit = 8,
         page = 1,
         search,
-      } = req.query as unknown as IPaginationQuery;
-
-      const query: FilterQuery<TEvent> = {};
-
-      if (search) {
-        Object.assign(query, {
-          ...query,
-          $text: {
-            $search: search,
-          },
-        });
-      }
-
+        category,
+        isOnline,
+        isFeatured,
+        isPublish,
+      } = req.query;
+      const query = buildQuery({
+        search,
+        category,
+        isFeatured,
+        isPublish,
+        isOnline,
+      });
       const result = await EventModel.find(query)
-        .limit(limit)
-        .skip((page - 1) * limit)
+        .limit(+limit)
+        .skip((+page - 1) * +limit)
         .sort({ createdAt: -1 })
+        .lean()
         .exec();
 
       const count = await EventModel.countDocuments(query);
@@ -47,9 +59,9 @@ export default {
         res,
         result,
         {
-          current: page,
+          current: +page,
           total: count,
-          totalPages: Math.ceil(count / limit),
+          totalPages: Math.ceil(count / +limit),
         },
         "success find all events"
       );
